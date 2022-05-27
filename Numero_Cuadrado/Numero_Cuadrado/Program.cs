f0aa94213a7af4e0c1e1289794ad25aefdd29ae8
IngSistemasHeranandez <105814871+IngSistemasHeranandez@users.noreply.github.com>
2022-05-27 00:44:04 -0700

    Add files via upload

---
+++ Numero_Cuadrado/Numero_Cuadrado/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Samuel Hernandez Perez 21210671
+namespace Numero_Cuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Salida de programa hasta que cumpla.
+            bool SalidaE = true;
+
+            //titulo
+            Console.Title = "NUMERO AL CUADRADO CON EXCEPCIONES";
+
+            //ciclo
+            do
+            {
+                //Limpiar consola para ingrsar nuevamente
+                Console.Clear();
+
+                //inicio de validadcion 
+                try
+                {
+                    //Asiganacion de valor a las variables
+                    Console.Write("Ingrese un numero entero: ");
+                    int numero = int.Parse(Console.ReadLine());
+
+                    //Calculo
+                    Console.WriteLine(numero + "^2 = " + (numero * numero));
+
+                    //Si no es correcta la validacion no permite la salida
+                    SalidaE = false;
+                }
+                //verificacion de error
+                catch (FormatException e)
+                {
+                    //Mensaje de error
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error");
+                    Console.ResetColor();
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.WriteLine("Presione enter para continuar. . .");
+                Console.ReadKey();
+
+             
+            } while (SalidaE == true);
+        }
+    }
+}
